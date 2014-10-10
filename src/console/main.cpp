8b779710236c0b7cc3bbc9d2945f9a0a4afb6298
Pavel Dolgov <pdolgov99@gmail.com>
2014-10-10 23:27:59 +0400

    Do not use excess pointers

--- src/console/main.cpp
+++ src/console/main.cpp
@@ -9,10 +9,8 @@ int main(int argc, char* argv[])
 {
     srand(time(NULL));
     GameDesk desk;
-    GameDesk *desk_point = &desk;
     IoBase io_base;
-    IoBase *io_base_point = &io_base;
-    io_base.setDesk(desk_point);
-    GameModes game_modes(desk_point, io_base_point);
+    io_base.setDesk(&desk);
+    GameModes game_modes(&desk, &io_base);
     game_modes.start(argc, argv);
 }
