08ccc7280099128565e119616eb204968499e4a6
Pavel Dolgov <pdolgov99@gmail.com>
2014-10-13 22:47:23 +0400

    Simplify the code using bool variables

--- src/controller/checker.cpp
+++ src/controller/checker.cpp
@@ -4,13 +4,9 @@
 
 bool Checker::checkStep(GameDesk& a, Points& points) {
     int rownumber = a.getRowNumber();
-    if (checkIndex(rownumber, points)) {
-        if (a.getDeskNumber(points.p1) == a.getDeskNumber(points.p2)) {
-            return true;
-        }
-        return false;
-    }
-    return false;
+    int n1 = a.getDeskNumber(points.p1);
+    int n2 = a.getDeskNumber(points.p2);
+    return checkIndex(rownumber, points) && n1 == n2;
 }
 
 bool Checker::checkIndex(int max, Points& points) {
@@ -34,12 +30,8 @@ bool Checker::checkRange(int max, Points& points) {
     int p2c = points.p2.col;
     int p1r = points.p1.row;
     int p2r = points.p2.row;
-    if ((p1r >= 0) && (p1c >= 0) && (p2r >= 0) && (p2c >= 0)) {
-        if ((p1r < max) && (p1c < max) && (p2r < max) && (p2c < max)) {
-            return true;
-        }
-        return false;
-    }
-    return false;
+    bool gt0 = p1r >= 0 && p1c >= 0 && p2r >= 0 && p2c >= 0;
+    bool ltm = p1r < max && p1c < max && p2r < max && p2c < max;
+    return gt0 && ltm;
 }
 
