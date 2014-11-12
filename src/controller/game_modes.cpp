e50108ffe93b2a6e4da78433c83ce827d1bf1b1d
Pavel Dolgov <pdolgov99@gmail.com>
2014-11-12 22:04:06 +0300

    Correct typos in GameModes::gameUndo

--- src/controller/game_modes.cpp
+++ src/controller/game_modes.cpp
@@ -175,7 +175,7 @@ void GameModes::gameUndo(Move& move, int steps_number) {
     if (move.undo_steps_number == 0) {
         return;
     }
-    prev_move = desk_->getStep(steps_number - 1);
+    Move prev_move = desk_->getStep(steps_number - 1);
     move.p1 = prev_move.p1;
     move.p2 = prev_move.p2;
     move.random_number = prev_move.random_number;
@@ -189,7 +189,7 @@ void GameModes::gameUndo(Move& move, int steps_number) {
         desk_->setDeskNumber(move.p1, number);
         move.p1.col++;
     }
-    desk_->setDeskNumber(move.p1; move.random_number);
+    desk_->setDeskNumber(move.p1, move.random_number);
     desk_->removeStep();
     move.undo_steps_number -= 1;
     gameUndo(move, steps_number - 1);
