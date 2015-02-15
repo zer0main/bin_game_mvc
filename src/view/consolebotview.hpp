b2e4f6d366fb06eae3cdad0af80e4a01b0c31074
Pavel Dolgov <pdolgov99@gmail.com>
2015-02-15 22:48:05 +0300

    Apply astyle

--- src/view/consolebotview.hpp
+++ src/view/consolebotview.hpp
@@ -6,11 +6,11 @@
 class ConsoleBotView: public IoView {
 protected:
     void outputGeneral() const;
-    
+
     void typeError_impl() const;
 
     void indexError_impl() const;
-    
+
     void finish_impl(bool check_fail, int score, int steps_number) const;
 
     void sendHelpMessage_impl() const;
