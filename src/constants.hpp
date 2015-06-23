0f023c6b065fcdb5b721cc1b4d0d434eba01ed6a
Pavel Dolgov <pdolgov99@gmail.com>
2015-06-23 22:16:30 +0300

    constants.hpp: new header for all the constants

---
+++ src/constants.hpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2014-2015 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef CONSTANTS_HPP_
+#define CONSTANTS_HPP_
+
+/** Minimum width/length of game board */
+const int MIN_WIDTH = 3;
+
+/** Maximum width/length of game board */
+const int MAX_WIDTH = 16;
+
+#endif
