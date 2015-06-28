1969b60d2ca4e3147a2e192b09ab887aa2e1d056
Pavel Dolgov <pdolgov99@gmail.com>
2015-06-28 12:47:21 +0300

    Add min/max number of minutes; score to constants

--- src/constants.hpp
+++ src/constants.hpp
@@ -13,4 +13,16 @@ const int MIN_WIDTH = 3;
 /** Maximum width/length of game board */
 const int MAX_WIDTH = 16;
 
+/** Minimum number of minutes to play */
+const int MIN_TIME = 1;
+
+/** Maximum number of minutes to play */
+const int MAX_TIME = 1000;
+
+/** Minimum winning score */
+const int MIN_SCORE = 10;
+
+/** Maximum winning score */
+const int MAX_SCORE = 9999999;
+
 #endif
