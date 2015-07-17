e1f1c201a40afd912f25d0a03021aed68d29174b
Pavel Dolgov <pdolgov99@gmail.com>
2015-07-17 17:22:31 +0300

    New structure GameOptions
    Special game parameters.

---
+++ src/gui/GameOptions.hpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2014-2015 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef GAME_OPTIONS_HPP
+#define GAME_OPTIONS_HPP
+
+#include "Points.hpp"
+
+enum GameType {
+    WIN_MODE;
+    TIME_MODE;
+    SCORE_MODE;
+};
+
+/** Special game parameters */
+struct GameOptions {
+    GameType game_type_;
+
+    int win_number_;
+    int time_number_;
+    int starting_time_;
+    int steps_number_;
+
+    bool first_click_;
+
+    Points current_step_;
+};
+
+#endif
