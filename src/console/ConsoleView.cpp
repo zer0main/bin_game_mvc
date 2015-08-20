db6bfec9474ad16b2c3c9b69f10e3d2edc76ea19
Pavel Dolgov <pdolgov99@gmail.com>
2015-08-21 00:01:23 +0300

    Use getInitialParameter() in ConsoleView's methods

--- src/console/ConsoleView.cpp
+++ src/console/ConsoleView.cpp
@@ -9,7 +9,6 @@
 #include "ConsoleView.hpp"
 
 int ConsoleView::getDeskSize_impl() const {
-    int deskSize;
     std::cout << "Please enter size of the game board."
               << std::endl;
     std::cout << "Your board will have x * x square."
@@ -21,60 +20,25 @@ int ConsoleView::getDeskSize_impl() const {
                  "corresponds to the size of your screen."
               << std::endl;
     prompt();
-    while (true) {
-        if (std::cin >> deskSize) {
-            if (checkRange(deskSize, BOARDS_SIZE)) {
-                break;
-            } else {
-                rangeError();
-            }
-        } else {
-            typeError();
-        }
-    }
-    return deskSize;
+    return getInitialParameter(BOARDS_SIZE);
 }
 
 int ConsoleView::getWinNumber_impl() const {
-    int winNumber;
     int square = getBoardsSquare();
     std::cout << "What score you want to finish the "
                  "game?" << std::endl;
     std::cout << "Minimum is " << square * 2 << " and "
                  "maximum is " << MAX_SCORE << std::endl;
     prompt();
-    while (true) {
-        if (std::cin >> winNumber) {
-            if (checkRange(winNumber, SCORE)) {
-                break;
-            } else {
-                rangeError();
-            }
-        } else {
-            typeError();
-        }
-    }
-    return winNumber;
+    return getInitialParameter(SCORE);
 }
 
 int ConsoleView::getTimeNumber_impl() const {
-    int timeNumber;
     std::cout << "How many minutes you want to play?" << std::endl;
     std::cout << "Minimum is " << MIN_TIME << " and "
                  "maximum is " << MAX_TIME << std::endl;
     prompt();
-    while (true) {
-        if (std::cin >> timeNumber) {
-            if (checkRange(timeNumber, TIME)) {
-                break;
-            } else {
-                rangeError();
-            }
-        } else {
-            typeError();
-        }
-    }
-    return timeNumber;
+    return getInitialParameter(TIME);
 }
 
 Points ConsoleView::getIndex_impl() const {
